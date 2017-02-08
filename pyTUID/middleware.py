cdd2fba2a46d7bfa8f9f7824935f7c87499780ba
Fabian Franke <fabian-franke@t-online.de>
2017-02-08 03:07:19 +0100

    Added middleware.
    Requests now have a 'TUIDUser' attribute which contains the logged in TUID User or None.

---
+++ pyTUID/middleware.py
@@ -0,0 +1,15 @@
+from .models import TUIDUser
+from .util import update_user
+from . import settings
+
+class TUIDMiddleware(object):
+    def process_request(self, request):
+        if 'TUID' in request.session:
+            if settings.TUID_CREATE_USER:
+                request.TUIDUser = TUIDUser.objects.get(uid=request.session['TUID'][0])
+            else:
+                tuid_user = TUIDUser()
+                update_user(tuid_user, *request.session['TUID'])
+                request.TUIDUser = tuid_user
+        else:
+            request.TUIDUser = None
